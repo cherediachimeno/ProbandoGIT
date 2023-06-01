de19eda759ef613b813c52f4e07c314d3f1259fc
Carlos H <98904939+cherediachimeno@users.noreply.github.com>
2023-06-01 11:48:22 +0200

    commit

---
+++ EXTRA/almacen.js
@@ -0,0 +1,18 @@
+let almacen = [
+  {
+    departamento: "mujer",
+    producto1: { nombre: "Chaqueta denim", precio: 175.9, stock: 5 },
+    producto2: { nombre: "Jeans", precio: 35.9, stock: 4 },
+    producto3: { nombre: "Zapatillas", precio: 89.9, stock: 2 },
+    producto4: { nombre: "Jersey", precio: 89.9, stock: 3 },
+  },
+  {
+    departamento: "hombre",
+    producto1: { nombre: "Chaqueta denim", precio: 175.9, stock: 4 },
+    producto2: { nombre: "Jeans", precio: 35.9, stock: 5 },
+    producto3: { nombre: "Zapatillas", precio: 89.9, stock: 1 },
+    producto4: { nombre: "Jersey", precio: 89.9, stock: 2 },
+  },
+];
+
+module.exports.almacen = almacen;
